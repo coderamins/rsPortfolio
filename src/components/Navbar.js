433d11b06ff3491cc49d4031fd6cbc8b6c6638e5
1007297 <1007297@1007297T1082753>
2021-09-14 15:52:07 +0430

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <button class="lg:dark:hover:bg-white ...">
+          dark mode
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
